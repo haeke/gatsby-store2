640bdf8031477c33c7f59e50df3b21b9067f0e58
haeke <riverae2@student.wpunj.edu>
2019-04-24 16:59:43 -0400

    added social icons to the footer, added the logo name to the footer along with links for navigation to the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,59 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import facebookLogo from "../images/facebook.svg"
+import instaLogo from "../images/insta.svg"
+import pinterestLogo from "../images/pinterest.svg"
 
 const Footer = () => {
   return (
-    <footer className="footer py-3">
-      <div className="container">
+    <footer className="footer orangeBackground py-3">
+      <div className="col-md-12">
         <div className="row">
-          <div className="col-10 mx-auto text-capitalize">
-            <h3 className="text-center">
+          <div className="col-md-6 text-capitalize text-center">
+            <h3 className="display-4 mb-3 brandColor">Camping Supplies</h3>
+            <h3 className="mainWhiteText">
               All Rights Reserved &copy; {new Date().getFullYear()}{" "}
             </h3>
+          </div>
+          <div className="col-md-6">
+            <div className="socials text-left">
+              <Link to="https://www.facebook.com">
+                <img
+                  src={facebookLogo}
+                  alt="facebook icon"
+                  style={{ height: 30, width: 30, margin: "20px 10px" }}
+                />
+              </Link>
+              <Link to="https://www.instagram.com">
+                <img
+                  src={instaLogo}
+                  alt="instagram icon"
+                  style={{ height: 30, width: 30, margin: "0 10px" }}
+                />
+              </Link>
+              <Link to="https://www.pinterest.com">
+                <img
+                  src={pinterestLogo}
+                  alt="pinterest icon"
+                  style={{ height: 30, width: 30, margin: "0 10px" }}
+                />
+              </Link>
+            </div>
+            <div className="links">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="mainWhiteText text-capitalize" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="mainWhiteText text-capitalize" to="/">
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
